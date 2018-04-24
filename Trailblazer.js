9a29ee82a9a3bd26d67a95146d94ed7210a9159e
Meighan Brodkey <meighanrockssf@gmail.com>
2018-04-23 17:35:44 -0700

    js

--- Trailblazer.js
+++ Trailblazer.js
@@ -8,10 +8,10 @@ var obj = JSON.parse(xmlHttp.responseText);
   
 var widgetDiv = '<div class="profilePanel"> <div> <img src="'+obj.Profile_Image+'" class="avatar" style="height: 50px;border-radius: 50%;"/> </div> <div> <a href="'+obj.Trailhead_URL+'" target="_Blank" style="text-transform: capitalize;">'+obj.Name+'</a> <br/> <div class="slds-truncate" title="'+obj.Title+'">'+obj.Title+'</div> <span class="badgeRank">'+obj.Rank+'</span> </div> <div style="margin-top: 5px;"> <table> <tr> <td style="text-align: center;">Badges</td> <td style="text-align: center;">Points</td>';
 
-widgetDiv += '</tr> <tr> <td style="text-align: center;font-size: 16px;"> <span >'+obj.Badges+'</span> </td> <td style="text-align: center;font-size: 16px;"> <span >'+obj.Points+'</span> </td> <td style="text-align: center;font-size: 16px;">';
+widgetDiv += '</tr> <tr> <td style="text-align: center;font-size: 16px;"> <span >'+obj.Badges+'</span> </td> <td style="text-align: center;font-size: 16px;"> <span >'+obj.Points+'</span> </td>';
 
 
-widgetDiv += '</td></tr> </table> </div> <div class="logos" style="margin-top: 5px;"> <a class="logoLink" href="'+obj.Trailhead_URL+'" target="_Blank"> <img src="https://trailhead.salesforce.com/assets/trailhead-flogo-1c99be34ac805c0cb128c1bb5e0cf6570943530f86063bebb1cf576c93c30069.png" class="avatar"/> </a>';
+widgetDiv += '</tr> </table> </div> <div class="logos" style="margin-top: 5px;"> <a class="logoLink" href="'+obj.Trailhead_URL+'" target="_Blank"> <img src="https://trailhead.salesforce.com/assets/trailhead-flogo-1c99be34ac805c0cb128c1bb5e0cf6570943530f86063bebb1cf576c93c30069.png" class="avatar"/> </a>';
 
 if( obj.Facebook != '' && obj.Facebook != null ){
 widgetDiv += '<a class="logoLink" href="'+obj.Facebook+'" target="_Blank"> <img src="https://grytics.com/wp-content/uploads/2015/01/1414386628_facebook_circle_color-512.png" class="avatar"/> </a>';
